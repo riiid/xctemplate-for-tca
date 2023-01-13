e940ef9a51c2cefaa3136c29ee90c07faa38f706
Yessen <y.yesen.y@gmail.com>
2023-01-13 19:06:28 +0900

    🚨remove spaces

--- templates/TCA with ReducerProtocol.xctemplate/___FILEBASENAME___.swift
+++ templates/TCA with ReducerProtocol.xctemplate/___FILEBASENAME___.swift
@@ -5,7 +5,7 @@ import Dependencies
 
 public struct ___VARIABLE_productName:identifier___: ReducerProtocol {
   @Dependency(\.environment___VARIABLE_productName:identifier___) private var environment
-  
+
   public init() {}
 
   public var body: some ReducerProtocol<State, Action> {
