b91ca8b4fff3a506865ab7aec059f00f664ebf75
Yessen <y.yesen.y@gmail.com>
2023-03-21 16:29:40 +0900

    🔥 remove template with AnyReducer

--- templates/Swift Files for TCA.xctemplate/WithCounterSample/___FILEBASENAME___Environment.swift
+++
@@ -1,6 +0,0 @@
-//___FILEHEADER___
-
-public struct ___FILEBASENAMEASIDENTIFIER___ {
-  public init() {
-  }
-}
--- templates/Swift Files for TCA.xctemplate/WithCounterSample/___FILEBASENAME___State.swift
+++
@@ -1,9 +0,0 @@
-//___FILEHEADER___
-
-public struct ___FILEBASENAMEASIDENTIFIER___: Equatable {
-  var counter: Int = 0
-
-  public init(counter: Int = 0) {
-    self.counter = counter
-  }
-}
